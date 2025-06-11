674b680068de7c925d87c36d61f3a9dedbdefb06
rieckypoerwadiredja <rieckysyarief@gmail.com>
2025-06-11 10:45:51 +0700

    env setting

--- web/frontend/src/utils/convercation.js
+++ web/frontend/src/utils/convercation.js
@@ -93,7 +93,7 @@ export const deleteAllConversation = () => {
 
 export const conversationPredict = async (text) => {
   try {
-    const response = await fetch("http://127.0.0.1:5000/predict", {
+    const response = await fetch(import.meta.env.VITE_PREDICT_API_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- web/frontend/vite.config.js
+++ web/frontend/vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import dotenv from "dotenv";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  define: {
+    "process.env.VITE_PREDICT_API_URL": JSON.stringify(
+      process.env.VITE_PREDICT_API_URL
+    ),
+  },
 });
